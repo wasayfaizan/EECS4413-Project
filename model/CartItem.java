beb9f0e73a0e3c70ff3797ef8dd9b307d81d07ae
SpideyfanX <63918402+SpideyfanX@users.noreply.github.com>
2024-12-03 15:07:34 -0500

    Create CartItem.java

---
+++ model/CartItem.java
@@ -0,0 +1,38 @@
+package model;
+
+public class CartItem {
+
+    private String productId;   // ID of the product
+    private String name;        // Product name
+    private BigDecimal price;   // Price per unit
+    private int quantity;       // Number of items in the cart
+
+    // Getters and setters
+    public String getProductId() {
+        return productId;
+    }
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+}
