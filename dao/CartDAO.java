cdd833c4c5e078c9b3764c2c2afe66d5b14a3076
SpideyfanX <63918402+SpideyfanX@users.noreply.github.com>
2024-12-04 18:36:16 -0500

    Create CartDAO.java

---
+++ dao/CartDAO.java
@@ -0,0 +1,11 @@
+package dao;
+
+import model.Cart;
+
+public interface CartDAO {
+    Cart getCartByUserId(int userId) throws Exception;
+
+    void addProductToCart(int cartId, int productId) throws Exception;
+
+    void removeProductFromCart(int cartId, int productId) throws Exception;
+}
