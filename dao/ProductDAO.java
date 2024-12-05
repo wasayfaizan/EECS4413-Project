5050e9596ae97e03d3991e9e4b2a1522a37ad3d5
SpideyfanX <63918402+SpideyfanX@users.noreply.github.com>
2024-12-04 19:13:58 -0500

    Create ProductDAO.java

---
+++ dao/ProductDAO.java
@@ -0,0 +1,15 @@
+package dao;
+import model.Product;
+import java.util.List;
+
+public interface ProductDAO {
+    void addProduct(Product product) throws Exception;
+
+    Product getProductById(int id) throws Exception;
+
+    List<Product> getAllProducts() throws Exception;
+
+    void updateProduct(Product product) throws Exception;
+
+    void deleteProduct(int id) throws Exception;
+}
